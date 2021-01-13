65c9bb9b5c78c8d00a29b09f59415dcaffef6596
Dennis A. Torres Rodriguez <d70rr3s@gmail.com>
2021-01-13 01:29:15 +0100

    [REFACTOR JSX/PROPS] A 'Hello world' React sample app

--- react-hola-mundo/script.jsx.js
+++ react-hola-mundo/script.jsx.js
@@ -1,14 +1,24 @@
-const FooBar = () => <p>Foo bar</p>
+const FooBar = (props) => {
+    const isEpic = (props.roll % 2) === 0;
+    return (
+        <p>{isEpic ? 'Wow! Has ganado un ' : 'Meh! Otro '}
+            <span className={isEpic ? 'epic' : 'normal'}>Foo bar</span>
+        </p>
+    );
+}
 
 const Heading = () => <h1>Hola, mundo!</h1>
 
 const HolaMundo = () => {
+    const classList = ['heading'];
+    const randomFlag = Math.floor(Math.random() * 100, 0);
+
     return (
-        <div className={'heading'}>
+        <div className={classList}>
             <Heading />
-            <FooBar />
+            <FooBar roll={randomFlag} />
         </div>
-    )
+    );
 }
 
 ReactDOM.render(React.createElement(HolaMundo), document.getElementById('hola-mundo'));
